a7ae165e827f70f4636c48d7f73a2e5f81a9df01
Troels Jensen <troelsfedderjensen@hotmail.com>
2019-03-26 16:12:02 +0100

    Implemented remove() and unit tests Refactored code

--- TreapTest/TreapUnitTest.cpp
+++ TreapTest/TreapUnitTest.cpp
@@ -1,80 +1,81 @@
 #include "pch.h"
 #include "../Treaps/Treap.h"
+#include <iostream>
 
 
-TEST(RotateLeft, NullPtr)
-{
-	TreapNode<char>* result;
-	TreapNode<char>* a = new TreapNode<char>('a', 0);
-	TreapNode<char>* b = new TreapNode<char>('b', 0, a);
-	TreapNode<char>* X = new TreapNode<char>('X', 0, a);
-	TreapNode<char>* Y = new TreapNode<char>('X', 0, b);
-	TreapNode<char>* Z = new TreapNode<char>('X', 0, b);
-
-	a->left = X;
-	a->right = b;
-	b->left = Y;
-	b->right = Z;
-
-	result = rotateLeft(a);
-
-	ASSERT_EQ(result, b);
-
-	ASSERT_EQ(b->parent, nullptr);
-	ASSERT_EQ(b->left, a);
-	ASSERT_EQ(b->right, Z);
-	ASSERT_EQ(a->parent, b);
-	ASSERT_EQ(a->left, X);
-	ASSERT_EQ(a->right, Y);
-
-	ASSERT_EQ(X->parent, a);
-	ASSERT_EQ(X->left, nullptr);
-	ASSERT_EQ(X->right, nullptr);
-	ASSERT_EQ(Y->parent, a);
-	ASSERT_EQ(Y->left, nullptr);
-	ASSERT_EQ(Y->right, nullptr);
-	ASSERT_EQ(Z->parent, b);
-	ASSERT_EQ(Z->left, nullptr);
-	ASSERT_EQ(Z->right, nullptr);
-
-
-}
-
-TEST(RotateRight, Reconnected)
-{
-	TreapNode<char>* result;
-	TreapNode<char>* a = new TreapNode<char>('a', 0);
-	TreapNode<char>* b = new TreapNode<char>('b', 0, a);
-	TreapNode<char>* X = new TreapNode<char>('X', 0, b);
-	TreapNode<char>* Y = new TreapNode<char>('X', 0, b);
-	TreapNode<char>* Z = new TreapNode<char>('X', 0, a);
-
-	a->left = b;
-	a->right = Z;
-	b->left = X;
-	b->right = Y;
-
-	result = rotateRight(a);
-
-	ASSERT_EQ(result, b);
-
-	ASSERT_EQ(b->parent, nullptr);
-	ASSERT_EQ(b->right, a);
-	ASSERT_EQ(b->left, X);
-	ASSERT_EQ(a->parent, b);
-	ASSERT_EQ(a->left, Y);
-	ASSERT_EQ(a->right, Z);
-
-	ASSERT_EQ(X->parent, b);
-	ASSERT_EQ(X->left, nullptr);
-	ASSERT_EQ(X->right, nullptr);
-	ASSERT_EQ(Y->parent, a);
-	ASSERT_EQ(Y->left, nullptr);
-	ASSERT_EQ(Y->right, nullptr);
-	ASSERT_EQ(Z->parent, a);
-	ASSERT_EQ(Z->left, nullptr);
-	ASSERT_EQ(Z->right, nullptr);
-}
+//TEST(RotateLeft, NullPtr)
+//{
+//	TreapNode<char>* result;
+//	TreapNode<char>* a = new TreapNode<char>('a', 0);
+//	TreapNode<char>* b = new TreapNode<char>('b', 0, a);
+//	TreapNode<char>* X = new TreapNode<char>('X', 0, a);
+//	TreapNode<char>* Y = new TreapNode<char>('X', 0, b);
+//	TreapNode<char>* Z = new TreapNode<char>('X', 0, b);
+//
+//	a->left = X;
+//	a->right = b;
+//	b->left = Y;
+//	b->right = Z;
+//
+//	result = rotateLeft(a);
+//
+//	ASSERT_EQ(result, b);
+//
+//	ASSERT_EQ(b->parent, nullptr);
+//	ASSERT_EQ(b->left, a);
+//	ASSERT_EQ(b->right, Z);
+//	ASSERT_EQ(a->parent, b);
+//	ASSERT_EQ(a->left, X);
+//	ASSERT_EQ(a->right, Y);
+//
+//	ASSERT_EQ(X->parent, a);
+//	ASSERT_EQ(X->left, nullptr);
+//	ASSERT_EQ(X->right, nullptr);
+//	ASSERT_EQ(Y->parent, a);
+//	ASSERT_EQ(Y->left, nullptr);
+//	ASSERT_EQ(Y->right, nullptr);
+//	ASSERT_EQ(Z->parent, b);
+//	ASSERT_EQ(Z->left, nullptr);
+//	ASSERT_EQ(Z->right, nullptr);
+//
+//
+//}
+//
+//TEST(RotateRight, Reconnected)
+//{
+//	TreapNode<char>* result;
+//	TreapNode<char>* a = new TreapNode<char>('a', 0);
+//	TreapNode<char>* b = new TreapNode<char>('b', 0, a);
+//	TreapNode<char>* X = new TreapNode<char>('X', 0, b);
+//	TreapNode<char>* Y = new TreapNode<char>('X', 0, b);
+//	TreapNode<char>* Z = new TreapNode<char>('X', 0, a);
+//
+//	a->left = b;
+//	a->right = Z;
+//	b->left = X;
+//	b->right = Y;
+//
+//	result = rotateRight(a);
+//
+//	ASSERT_EQ(result, b);
+//
+//	ASSERT_EQ(b->parent, nullptr);
+//	ASSERT_EQ(b->right, a);
+//	ASSERT_EQ(b->left, X);
+//	ASSERT_EQ(a->parent, b);
+//	ASSERT_EQ(a->left, Y);
+//	ASSERT_EQ(a->right, Z);
+//
+//	ASSERT_EQ(X->parent, b);
+//	ASSERT_EQ(X->left, nullptr);
+//	ASSERT_EQ(X->right, nullptr);
+//	ASSERT_EQ(Y->parent, a);
+//	ASSERT_EQ(Y->left, nullptr);
+//	ASSERT_EQ(Y->right, nullptr);
+//	ASSERT_EQ(Z->parent, a);
+//	ASSERT_EQ(Z->left, nullptr);
+//	ASSERT_EQ(Z->right, nullptr);
+//}
 
 
 TEST(Treap, Search_EmptyTreap_DataNotFound)
@@ -93,7 +94,7 @@ TEST(Treap, Search_OneElementInserted_DataFound)
 }
 
 
-TEST(Treap, Search_TwoElementsInserted_BothFound)
+TEST(Treap, Insert_TwoElementsInserted_BothFound)
 {
 	Treap<int> uut;
 
@@ -104,7 +105,7 @@ TEST(Treap, Search_TwoElementsInserted_BothFound)
 }
 
 
-TEST(Treap, Search_ThreeElementsInserted_AllFound)
+TEST(Treap, Insert_ThreeElementsInserted_AllFound)
 {
 	Treap<int> uut;
 
@@ -115,6 +116,7 @@ TEST(Treap, Search_ThreeElementsInserted_AllFound)
 	ASSERT_TRUE(uut.search(0));
 	ASSERT_TRUE(uut.search(1));
 	ASSERT_TRUE(uut.search(2));
+	ASSERT_TRUE(uut.search(3));
 }
 
 
@@ -122,11 +124,102 @@ TEST(Treap, Search_100ElementsInserted_AllFound)
 {
 	Treap<int> uut;
 	int n = 100;
-
+	bool ok = true;
 	for(int i=0; i< n; i++)	uut.insert(i);
 
 	for (int i = 0; i< n; i++)
-		if (!uut.search(i)) ASSERT_TRUE(false);
+		if (!uut.search(i))
+		{
+			ok &= false;
+		}
 
-	ASSERT_TRUE(true);
+	ASSERT_TRUE(ok);
+}
+
+
+TEST(Treap, Remove_OneElementInsertedThenRemoved_DataNotFound)
+{
+	Treap<int> uut;
+
+	uut.insert(2);
+	uut.remove(2);
+	ASSERT_FALSE(uut.search(2));
+}
+
+TEST(Treap, Remove_OneElementInsertedAnotherRemoved_DataFound)
+{
+	Treap<int> uut;
+
+	uut.insert(2);
+	uut.remove(3);
+	ASSERT_TRUE(uut.search(2));
+}
+
+TEST(Treap, Remove_TwoElementInsertedOneRemoved_OtherDataStillFound)
+{
+	Treap<int> uut;
+
+	uut.insert(2);
+	uut.insert(3);
+	uut.remove(3);
+	ASSERT_TRUE(uut.search(2));
+}
+
+TEST(Treap, Remove_TwoElementInsertedOneRemoved_DataNotFound)
+{
+	Treap<int> uut;
+
+	uut.insert(2);
+	uut.insert(3);
+	uut.remove(3);
+	ASSERT_FALSE(uut.search(3));
+}
+
+
+TEST(Treap, Search_ManyElementsInsertedEveryOtherRemoved_OthersFound)
+{
+	Treap<int> uut;
+	int n = 200;
+	bool ok = true;
+	for (int i = 0; i < n; i++)	
+		uut.insert(i);
+
+	for (int j = 0; j < n; j += 2)
+	{
+		uut.remove(j);
+	}
+
+
+	for (int k = 1; k < n; k += 2)
+	{
+		if (!uut.search(k))
+		{
+			ok &= false;
+		}
+
+		ASSERT_TRUE(ok);
+	}
+}
+
+
+TEST(Treap, Search_ManyElementsInsertedEveryOtherRemoved_RemovedElementsNotFound)
+{
+	Treap<int> uut;
+	int n = 100;
+	bool ok = true;
+	for (int i = 0; i < n; i++)
+		uut.insert(i);
+
+	for (int j = 0; j < n; j += 2)
+		uut.remove(j);
+
+	for (int k = 0; k < n; k += 2)
+	{
+		if (uut.search(k))
+		{
+			ok &= false;
+		}
+
+		ASSERT_TRUE(ok);
+	}
 }
--- Treaps/Treap.h
+++ Treaps/Treap.h
@@ -1,84 +1,108 @@
 #pragma once
 #include <cstdlib>
 #include <ctime>
-
-// internal class to represent a Treap node
-template<typename T>
-class TreapNode
-{
-public:
-	T data;
-	int pri;
-	TreapNode* parent;
-	TreapNode* left;
-	TreapNode* right;
-
-	TreapNode(const T& d = T(), const int pri = 0, TreapNode* p = nullptr, TreapNode* l = nullptr,
-		TreapNode* r = nullptr) :
-		data(d), pri(pri), parent(p), left(l), right(r)
-	{
-	}
-};
-
-template<typename T>
-TreapNode<T>* rotateLeft(TreapNode<T>* b)
-{
-	if (b == nullptr || b->right == nullptr) return b;
-	TreapNode<T>* c = b->right;
-	b->right = c->left;
-	if (b->right) b->right->parent = b;
-	c->left = b;
-	c->parent = b->parent;
-	b->parent = c;
-
-	return c;
-}
-
-
-/* Returns pointer to c (new root after rotation)
-*
-*	       b              c
-*	      / \            / \
-*	     c   X    ->    Y   b
-*	    / \                / \
-*	   Y   Z              Z   X
-*
-*/
-
-template<typename T>
-TreapNode<T>* rotateRight(TreapNode<T>* b)
-{
-	if (b == nullptr || b->left == nullptr) return b;
-	TreapNode<T>* c = b->left;
-	b->left = c->right;
-	if (b->left) b->left->parent = b;
-	c->right = b;
-	c->parent = b->parent;
-	b->parent = c;
-
-	return c;
-}
+#include <ostream>
 
 
 template <typename T>
 class Treap
 {
-public:
-	int generatePriority() const
+private:
+	// internal class to represent a Treap node
+	template<typename T>
+	class TreapNode
 	{
-		return rand() % (MAX_PRI - MIN_PRI + 1) + MIN_PRI;
+	public:
+		T data;
+		int pri;
+		TreapNode* parent;
+		TreapNode* left;
+		TreapNode* right;
+
+		TreapNode(const T& d = T(), const int pri = 0, TreapNode* p = nullptr, TreapNode* l = nullptr,
+			TreapNode* r = nullptr) :
+			data(d), pri(pri), parent(p), left(l), right(r)
+		{
+		}
+	};
+
+
+	template<typename T>
+	TreapNode<T>* rotateLeft(TreapNode<T>* b)
+	{
+		if (b == nullptr || b->right == nullptr) return b;
+		TreapNode<T>* c = b->right;
+		b->right = c->left;
+		if (b->right) b->right->parent = b;
+		c->left = b;
+		c->parent = b->parent;
+		b->parent = c;
+
+		return c;
+	}
+
+	// rotateRight()
+	// Returns pointer to c (new root after rotation)
+	//
+	//	       b              c
+	//	      / \            / \
+	//	     c   X    ->    Y   b
+	//	    / \                / \
+	//	   Y   Z              Z   X
+	//
+	//
+	template<typename T>
+	TreapNode<T>* rotateRight(TreapNode<T>* b)
+	{
+		if (b == nullptr || b->left == nullptr) return b;
+		TreapNode<T>* c = b->left;
+		b->left = c->right;
+		if (b->left) b->left->parent = b;
+		c->right = b;
+		c->parent = b->parent;
+		b->parent = c;
+
+		return c;
+	}
+
+
+	TreapNode<T>* find(const T& data)
+	{
+		TreapNode<T>* cursor = root;
+		while (true)
+		{
+			if (cursor == nullptr) return nullptr;
+			if (cursor->data == data) return cursor;
+			if (data < cursor->data) cursor = cursor->left;
+			else cursor = cursor->right;
+		}
+	}
+
+	int p = 0;
+
+
+	int generatePriority() 	{
+		int pri = rand() % (MAX_PRI - MIN_PRI + 1) + MIN_PRI;
+		return pri;
+		//return (p*p)%9;
 	}
 
 	int count = 0;
-	static const int MIN_PRI = 1;
-	static const int MAX_PRI = 100;
+	const int MIN_PRI = 1;
+	const int MAX_PRI = 100;
+
+
+	bool isLeftChild(TreapNode<T>* n) { return n != root && n->parent->left == n; }
+	bool isLeaf(TreapNode<T>* n) { return n->left == nullptr && n->right == nullptr;}
+
+public:
 
 	TreapNode<T>* root;
 
 	Treap()
 	{
 		root = nullptr;
-		srand((int) time(nullptr));
+		srand(static_cast<int>(time(nullptr)));
 	}
 
 	void insert(const T& data)
@@ -149,19 +173,19 @@ public:
 				TreapNode<T>* cursorParent = cursor->parent;
 				if (child == cursor->right)	// Need left rotation
 				{
-					if (cursor == cursorParent->right) 
-						cursorParent->right = rotateLeft(cursor);
+					if (isLeftChild(cursor)) 
+						cursorParent->left = rotateLeft(cursor); 
 					else 
-						cursorParent->left = rotateLeft(cursor);
+						cursorParent->right = rotateLeft(cursor);
 				}
 				else // Need right rotation
 				{
-					if (cursor == cursorParent->right) 
-						cursorParent->right = rotateRight(cursor);
-					else 
+					if (isLeftChild(cursor)) 
 						cursorParent->left = rotateRight(cursor);
+					else 
+						cursorParent->right = rotateRight(cursor);
 				}
-				cursor = cursorParent;
+				cursor = cursor->parent;
 			}
 			else
 				break;
@@ -171,21 +195,88 @@ public:
 
 	void remove(const T& data)
 	{
-		// TODO
+		TreapNode<T>* toKill = find(data);
+		if(toKill == nullptr) return;
+
+		//INV: toKill points to node to remove
+
+		if (isLeaf(toKill))
+		{
+			if (toKill == root)
+			{
+				root = nullptr;
+				delete toKill;
+				return;
+			}
+			// INV: toKill is not root
+			if (isLeftChild(toKill)) toKill->parent->left = nullptr;
+			else toKill->parent->right = nullptr;
+			delete toKill;
+			return;
+		}
+
+		// INV: toKill is non-leaf
+		if (toKill->left == nullptr || toKill->right == nullptr)
+		{
+			// INV: toKill has a single child
+			TreapNode<T>* singleChild = (toKill->left != nullptr ? toKill->left : toKill->right);
+
+			if (toKill == root) root = singleChild;
+			else
+			{
+				if (isLeftChild(toKill)) toKill->parent->left = singleChild;
+				else toKill->parent->right = singleChild;
+				singleChild->parent = toKill->parent;
+			}
+			delete toKill;
+			return;
+		}
+			
+
+
+		// INV: toKill has two children
+		if (toKill == root)
+			// INV: toKill is root
+			root = (toKill->left->pri > toKill->right->pri ? rotateRight(toKill) : rotateLeft(toKill));
+
+		
+		while(toKill->left != nullptr && toKill->right != nullptr)	// toKill has two children - rotate further down in treap, always AWAY from higher-priority child
+		{
+			TreapNode<T>* toKillParent = toKill->parent;
+
+			// INV: toKill is non-root
+			if (toKill->left->pri > toKill->right->pri)
+			{
+				// Left child must become root => RIGHT rotation needed
+				if (isLeftChild(toKill)) toKillParent->left = rotateRight(toKill);
+				else toKillParent->right = rotateRight(toKill);
+			}
+			else
+			{
+				// Right child must become root => LEFT rotation needed
+				if (isLeftChild(toKill)) toKillParent->left = rotateLeft(toKill);
+				else toKillParent->right = rotateLeft(toKill);
+			}
+		}
+		
+		// Reconnect single child and remove
+		TreapNode<T>* singleChild = (toKill->left != nullptr ? toKill->left : toKill->right);
+
+		if (isLeftChild(toKill))
+			toKill->parent->left = singleChild;
+		else 
+			toKill->parent->right = singleChild;
+
+		singleChild->parent = toKill->parent;
+		delete toKill;
 	}
 
 
 	bool search(const T& data)
 	{
-		TreapNode<T>* cursor = root;
-		{
-			while (cursor)
-			{
-				if (cursor->data == data) return true;
-				if (data < cursor->data) cursor = cursor->left;
-				else cursor = cursor->right;
-			}
-			return false;
-		}
+		return find(data) != nullptr;
 	}
+
+
+
 };
